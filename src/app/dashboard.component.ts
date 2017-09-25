ac93031d5625cbed5f2b096fb4d431d0398eac86
Ken <kzl5010@gmail.com>
2017-09-25 13:42:30 -0700

    found mistake

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -5,8 +5,8 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  template: '<h3>Hello</h3>',
-  // templateUrl: './dashboard.component.html',
+  // template: '<h3>Hello</h3>',
+  templateUrl: './dashboard.component.html',
 })
 
 
@@ -14,12 +14,11 @@ export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  // constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
-    //
-    // this.heroService.getHeroes()
-    //   .then(heroes => this.heroes = heroes.slice(1, 5));
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
 
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,29 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 import { Location } from '@angular/common';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
   selector: 'hero-detail',
-  template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-    </div>
-  `
+  templateUrl: './hero-detail.component.html',
 })
 
 export class HeroDetailComponent {
   @Input() hero: Hero;
 
+  goBack(): void {
+    this.location.back();
+  }
   constructor(
     private heroService: HeroService,
     private route: ActivatedRoute,
     private location: Location
   ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.switchMap((params: ParamMap) =>
+  this.heroService.getHero(+params.get('id'))).subscribe(hero => this.hero = hero);
+  }
 }
